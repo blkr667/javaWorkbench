e04a8a87c90a00e80afe3a5a5f48aedf7d8b0908
blkr <ghut668@gmail.com>
2018-03-31 19:44:07 +0200

    stream reduce test

--- src/test/java/apis/standard/stream/StreamTest.java
+++ src/test/java/apis/standard/stream/StreamTest.java
@@ -3,6 +3,7 @@ package apis.standard.stream;
 import static org.junit.Assert.*;
 
 import java.util.Arrays;
+import java.util.Optional;
 import java.util.stream.Stream;
 import org.junit.Test;
 
@@ -15,9 +16,8 @@ import org.junit.Test;
 
 public class StreamTest {
 
-
 	@Test 
-	public void shouldFilterStream() {
+	public void shouldFilter() {
 		Stream<String> namesStream =
 			    Arrays.asList("Tom", "John", "Glenn", "Gilbert").stream();
 		
@@ -27,12 +27,54 @@ public class StreamTest {
 	}
 	
 	@Test 
-	public void shouldMapStream() {
+	public void shouldMap() {
 		Stream<String> namesStream =
 			    Arrays.asList("Tom", "John").stream();
 		
 		Stream<String> resultStream = namesStream.map(String::toUpperCase);
 
 		assertEquals(true, resultStream.allMatch(s -> s.equals("TOM") || s.equals("JOHN")));
+	}
+	
+	@Test 
+	public void shouldReduce_withAccumulator() {
+		Stream<Integer> namesStream =
+			    Arrays.asList(1, 2, 3).stream();
+		
+		Optional<Integer> reductionResult = namesStream.reduce(
+				(reduceState, currentElement) -> reduceState + currentElement);
+
+		assertEquals(new Integer(6), reductionResult.get());
+	}
+	
+	@Test 
+	public void shouldReduce_withAccumulator_identity() {
+		int identityValue = 0;
+		Stream<Integer> namesStream =
+			    Arrays.asList(1, 2, 3).stream();
+		
+		int reductionResult = namesStream.reduce(
+				identityValue,
+				/* accummulutar */(reduceState, currentElement) -> reduceState + currentElement);
+
+		assertEquals(6, reductionResult);
+	}
+	
+	/*
+	 * reduce(identity, accumulator, combiner) useful when parallelStream()
+	 * combiner combine two accumulated elements
+	 */
+	@Test 
+	public void shouldReduce_withAccumulator_identity_combiner() {
+		int identityValue = 0;
+		Stream<Integer> namesStream =
+			    Arrays.asList(1, 2, 3).parallelStream();
+		
+		int reductionResult = namesStream.reduce( 
+				identityValue,
+				/* accummulutar */(reduceState, currentElement) -> reduceState + currentElement,
+				/* combiner */(currentAccumulatedElement, otherAccumulatedElement) -> currentAccumulatedElement + otherAccumulatedElement);
+
+		assertEquals(6, reductionResult);
 	}
 }
