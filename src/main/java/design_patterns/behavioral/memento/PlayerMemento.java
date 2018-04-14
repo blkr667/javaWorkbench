4149c6fdf135c310836cd48026e97f3f5f871440
blkr <ghut668@gmail.com>
2018-04-14 13:13:10 +0200

    memento design pattern

---
+++ src/main/java/design_patterns/behavioral/memento/PlayerMemento.java
@@ -0,0 +1,13 @@
+package design_patterns.behavioral.memento;
+
+public class PlayerMemento {
+	private final PlayerState state;
+	
+	public PlayerMemento(PlayerState state) {
+		this.state = state;
+	}
+
+	public PlayerState getLastState() {
+		return state;
+	}
+}
---
+++ src/main/java/design_patterns/behavioral/memento/PlayerUnit.java
@@ -0,0 +1,24 @@
+package design_patterns.behavioral.memento;
+
+public class PlayerUnit {
+	private PlayerState state;
+	private PlayerMemento memento;
+
+	public void saveState() {
+		memento = new PlayerMemento(state);
+	}
+	
+	public void loadState() {
+		if(memento != null) {
+			state = memento.getLastState();
+		}
+	}
+	
+	public PlayerState getState() {
+		return state;
+	}
+
+	public void setState(PlayerState state) {
+		this.state = state;
+	}
+}
